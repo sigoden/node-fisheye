2d61d714796da9e65ec765f15bc1559488c9adc1
Sigoden Huang <sigoden@gmail.com>
2018-08-31 10:51:03 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,14 @@ export type Vet4d = Array<Number>;
 type Opaque<T, K> = T & { __opaque__: K };
 type Int = Opaque<number, "Int">;
 
+// Calibrate result
+interface KD {
+  // Camera matrix
+  K: Matx33d;
+  // Input vector
+  D: Vet4d;
+}
+
 /**
  * Performs camera calibaration
  * @param images - The batch checkboard images used to calibrate.
@@ -15,21 +23,21 @@ export function calibrate(
   images: Buffer[],
   checkboardWidth: Int,
   checkboardHeight: Int
-);
+): KD;
 
 // Options to control the generation of undistorted image.
 interface UndistortExtra {
   // Format of the dest image, use extname `.jpg`, `.png` to repersent
-  ext?: string,
+  ext?: string;
   /**
    * Quantity of the dest image
    * For JPEG, it can be a quality ( CV_IMWRITE_JPEG_QUALITY ) from 0 to 100 (the higher is the better). Default value is 95.
    * For PNG, it can be the compression level ( CV_IMWRITE_PNG_COMPRESSION ) from 0 to 9. A higher value means a smaller size and longer compression time. Default value is 3.
    * For WEBP, it can be a quality ( CV_IMWRITE_WEBP_QUALITY ) from 1 to 100 (the higher is the better). By default (without any parameter) and for quality above 100 the lossless compression is used.
    */
-  quantity?: Int,
-  // Scale of the dest image 
-  scale?: number,
+  quantity?: Int;
+  // Scale of the dest image
+  scale?: number;
 }
 
 /**
@@ -39,4 +47,9 @@ interface UndistortExtra {
  * @param D - Input vector of distortion coefficients \f$(k_1, k_2, k_3, k_4)\f$.
  * @param extra - Control how the undistorted image generated.
  */
-export function undistort(image: Buffer, K: Matx33d, D: Vet4d, extra?: UndistortExtra);
+export function undistort(
+  image: Buffer,
+  K: Matx33d,
+  D: Vet4d,
+  extra?: UndistortExtra
+): Buffer;
